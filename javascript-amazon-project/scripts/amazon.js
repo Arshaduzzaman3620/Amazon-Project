558ff990832bd44774a16625f801efd272948b81
Arshad3620 <ridoy3620@gmail.com>
2025-03-13 22:13:45 +0900

    import product from product js

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,6 +1,7 @@
 //import { cart as myCart } from '../data/cart.js';
 //import { products } from "../data/products.js";
 import {cart} from '../data/cart.js';
+import { products } from '../data/products.js';
 
 let productsHTML = '';
 
