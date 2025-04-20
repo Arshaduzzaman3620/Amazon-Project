6c72e79922251bff45b2d7b865caa4af08bc521d
Arshad3620 <ridoy3620@gmail.com>
2025-04-20 17:25:59 +0900

    Save data the generate the html

--- javascript-amazon-project/data/deliveryOption.js
+++ javascript-amazon-project/data/deliveryOption.js
@@ -13,3 +13,13 @@
   priceCents:999
 }
 ];
+
+function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+  return deliveryOption || deliveryOption[0];
+}
