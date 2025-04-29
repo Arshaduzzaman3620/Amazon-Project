ae19843a49b2a86bb93570c221c2ea1b00ae40f2
Arshad3620 <ridoy3620@gmail.com>
2025-04-29 11:04:05 +0900

    add the oop

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,5 +1,8 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
+import '../dataa/cart-oop.js';
+
+
 renderOrderSummary();
 renderPaymentSummary();
